4c0fb0d9f9e0fedd8112b9698ea5404119d78369
vishnugops <vishvargops@gmail.com>
2024-01-19 15:06:46 -0800

    executable running

--- github_activity.py
+++ github_activity.py
@@ -5,10 +5,20 @@ from datetime import datetime, timedelta
 import random
 import time
 
+
 # Configure logging
 logging.basicConfig(filename='C:/Users/Vishnu-Server/Desktop/Coding/Activity-Influencer/activity_log.txt', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
-logging.info(f'Github Activity Influencer Started: Logging all info')
+
+
+def print_and_log(str):
+    logging.info(str)
+    print(str)
+
+
+print_and_log(
+    f'****************************Github Activity Influencer Started****************************')
+
 
 # Your GitHub personal access token - Generate one in your GitHub account settings
 ACCESS_TOKEN = os.getenv('GITHUB_ACCESS_TOKEN')
@@ -30,10 +40,10 @@ file_name = 'activity.txt'
 def update_and_delete_file():
     while True:
         today = datetime.now()
-        logging.info(f'Checking date and time on {today}')
+        print_and_log(f'Checking date and time on {today}')
         # Check if it's 11:50 pm
         if today.hour == 23 and today.minute == 50:
-            logging.info(f'It is 11:50, checking commits at : {today} ')
+            print_and_log(f'It is 11:50, checking commits at : {today} ')
             # Check the number of commits for the day
             commits_today = list(repo.get_commits(since=today.replace(hour=0, minute=0, second=0, microsecond=0),
                                                   until=today.replace(hour=23, minute=59, second=59, microsecond=999999)))
@@ -41,7 +51,7 @@ def update_and_delete_file():
             if len(commits_today) == 0:
                 # If no commits for the day, generate random commits between 2 and 16
                 num_commits = random.randint(2, 16)
-                logging.info(f'Random number of commints : {num_commits}')
+                print_and_log(f'Random number of commits : {num_commits}')
 
                 for i in range(num_commits):
                     # Randomly pick a time within the last 10 days
@@ -54,22 +64,22 @@ def update_and_delete_file():
                     repo.update_file(contents.path, commit_message,
                                      new_content, contents.sha, branch='main')
 
-                    logging.info(f'Committed change {i + 1} on {commit_date}')
+                    print_and_log(f'Committed change {i + 1} on {commit_date}')
             else:
-                logging.info(
-                    f'Commits already made for today: {len(commits_today)}')
+                print_and_log(
+                    f'Commits already made for today - {today} : {len(commits_today)}')
 
             # Calculate the sleep duration until the next day
             sleep_seconds = (23 * 3600) + (55 * 60)
-            logging.info(
+            print_and_log(
                 f'Waiting for {sleep_seconds} seconds until 11:50 pm next day...')
             time.sleep(sleep_seconds)
         else:
-            logging.info("It's not 11:50, not checking for commits")
+            print_and_log("It's not 11:50, not checking for commits")
             # Calculate the sleep duration until it's 11:50 pm
             sleep_seconds = abs((23 - today.hour) * 3600 +
                                 (50 - today.minute) * 60)
-            logging.info(
+            print_and_log(
                 f'Waiting for {sleep_seconds} seconds until today 11:50 pm...')
             time.sleep(sleep_seconds)
 
