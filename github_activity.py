d7e34b8c96976a459e9b30e56cc2a937cc0c3eb8
Vishnu Gopal Rajan <35251589+vishnuGops@users.noreply.github.com>
2024-04-30 21:35:05 -0700

    misc

--- github_activity.py
+++ github_activity.py
@@ -7,7 +7,6 @@ import pytz
 import gc  # Importing garbage collector module
 
 from github import Github
-from dotenv import load_dotenv
 
 # Version of the app
 
@@ -124,8 +123,6 @@ def number_from_normal_curve(min_value, max_value, mu, sigma):
 # Main code
 def generate_activity_based_on_commits():
     display_version()
-    load_dotenv()  # Load environment variables from .env file
-    # ACCESS_TOKEN = os.getenv('GITHUB_API_KEY')
     ACCESS_TOKEN = read_api_key(
         'C:\\Users\\Vishnu-Server\\Desktop\\Coding\\Activity-Influencer\\token.env')
     repo_owner = 'vishnugops'
