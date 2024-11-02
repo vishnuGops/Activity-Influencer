f639b4246a7be3177d5d23518a8f27af0c49d0e6
Vishnu <vishvargops@gmail.com>
2024-11-01 20:20:19 -0700

    structure change

--- main.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-from config import setup_folders
-from app_ui import AppUI
-
-# Run the Tkinter app
-setup_folders()  # Set up folders on startup
-root = Tk()
-app = AppUI(root)
-root.mainloop()
